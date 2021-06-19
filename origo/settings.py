4d94cb9399705282af546d41af46b15c42337cbd
RoomierCG <alex.comcg@gmail.com>
2021-06-19 19:42:17 +0200

    SECRET_KEY is env variable

--- origo/settings.py
+++ origo/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Incializacion de variables de entorno
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qh$j3sxxo(c57iqb*o@^(z4yb##&#$i928!*idw4equ+ngsfa3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
